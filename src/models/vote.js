e91793499f4da5c60dead93048b44e393bc1acd3
rosetta-jpn <rosetta.jpn@gmail.com>
2014-07-11 23:20:49 +0900

    Vote

---
+++ src/models/vote.js
@@ -0,0 +1,83 @@
+States = ["select_member", "vote", "agree","disagree"]
+
+function Vote(selector,group_sz,voter_sz){
+    this.selector = selector;
+    this.group = [];// User Array
+    this.group_sz = group_sz;
+    this.voter_map = {};//Voter map User-> Yes/No(true/false)
+    this.voter_sz = voter_sz;
+    this.vote_count = 1;
+    this.state = States[0];
+}
+
+Vote.prototype.add_group = function(pusher,add_man) {
+    if(this.selector === pusher && this.group.length < group_sz){
+        var add_man_in_group = false;
+        for(var i = 0; i < group.length;i++){
+            if(this.group[i] === add_man){
+                add_man_in_group = true;
+            }
+        }
+        if(!add_man_in_group){
+            this.group.push(add_man);
+        }
+    }
+}
+
+Vote.prototype.remove_group = function(remover,remove_man){
+    if(this.selector === remover && this.group.length > 0){
+        var remove_man_index = -1;
+        for(var i = 0; i < group.length;i++){
+            if(this.group[i] === remove_man){
+                remove_man_index = i;
+            }
+        }
+        if(remove_man_index > 0){
+            this.group = this.group.splice(remove_man_index,1);
+        }
+    }
+}
+
+Vote.prototype.is_vote_success = function(){
+    var agree = 0;
+    var disagree = 0;
+    if(Object.keys(this.voter_map).length != voter_sz){
+        return false;
+    }
+    for(var pep in voter_map){
+        if(voter_map[pep]){
+            agree += 1;
+        }else{
+            disagree += 1;
+        }
+    }
+    return agree > disagree;
+}
+
+Vote.prototype.change_voter_map(voter,vote_res){
+    
+    voter_map[voter.id] = vote_res;
+
+}
+
+Vote.prototype.go_vote(){
+    if(this.state === States[0] && this.group.length == this.group_sz){
+        this.state = States[1];
+    }
+}
+
+Vote.prototype.judge(){
+    if(this.state == States[1] && Object.keys(this.voter_map).length == voter_sz){
+        this.state = States[2];
+}
+    if(this.state == States[2]){
+        if(this.is_vote_success()){
+            this.state = States[3];
+        }else{
+            this.state = States[2];
+        }
+    }
+}
+
+
+module.exports = Vote;
