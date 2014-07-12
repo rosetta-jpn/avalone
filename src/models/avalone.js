1cc0955b0f364c0a18ce315827e7e57471689a6c
Tomoya Chiba <tomo.asleep@gmail.com>
2014-07-12 21:33:57 +0900

    Add callback functions to Avalone

--- src/models/avalone.js
+++ src/models/avalone.js
@@ -8,17 +8,17 @@ var Avalone = module.exports = function Avalone(connectorConstructor) {
   this.connector = connectorConstructor(this);
   this.rooms = [];
   this.users = {};
-
   this.on('enter', this.onEnter.bind(this))
 }
 
 utils.inherit(events.EventEmitter, Avalone);
 utils.extend(Avalone.prototype, roomModule('users'));
 
-Avalone.prototype.createRoom = function (owner) {
-  var room = new Room(owner);
+Avalone.prototype.createRoom = function (owner, name) {
+  var room = new Room(owner, name);
   this.rooms.push(room);
-  room.on('destroy', this.removeRoom(room).bind(this, room));
+  this.trigger('createRoom', room);
+  room.on('destroy', this.removeRoom.bind(this, room));
 }
 
 Avalone.prototype.removeRoom = function (room) {
@@ -29,8 +29,25 @@ Avalone.prototype.removeRoom = function (room) {
 Avalone.prototype.login = function (socket, id) {
   user = new User(id, id, socket);
   this.enter(user);
+  user.on('rename', this.onUserRename.bind(this, user));
 }
 
 Avalone.prototype.onEnter = function (user) {
-  this.connector.broadcast('login', user.toString());
+  this.connector.notice('login', user.toString());
+}
+
+Avalone.prototype.onUserRename = function (user, name) {
+  this.connector.notice('renameUser', name);
+}
+
+Avalone.prototype.createRoomCallback = function (room) {
+  this.connector.notice('createRoom', room.toString());
+}
+
+Avalone.prototype.trigger = function (type, data) {
+  this.emit(type, data);
+  var callback = this[type + "Callback" ];
+  if (callback) {
+    callback.call(this, data);
+  }
 }
