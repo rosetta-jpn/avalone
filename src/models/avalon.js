01f07a442f4aa4a77224467f84e90535f5b76164
Tomoya Chiba <tomo.asleep@gmail.com>
2014-07-14 21:48:12 +0900

    Observe avalon with AvalonObserver

--- src/models/avalon.js
+++ src/models/avalon.js
@@ -5,22 +5,20 @@ var utils = require('../utils')
   , RoomObserver = require('../observers/room_observer')
   , roomModule = require('../utils/room_module');
 
-var Avalon = module.exports = function Avalon(connectorConstructor) {
-  this.connector = connectorConstructor(this);
+var Avalon = module.exports = function Avalon() {
   this.rooms = {};
   this.users = {};
-  this.on('enter', this.onEnter.bind(this))
 }
 
 utils.inherit(events.EventEmitter, Avalon);
 utils.extend(Avalon.prototype, roomModule('users'));
+
 utils.extend(Avalon.prototype, {
   createRoom: function (owner, name) {
     if (this.rooms[name]) throw new Error('Room: ' + name + ' is already exists.');
     var room = new Room(owner, name);
     this.rooms[room.name] = room;
-    new RoomObserver(room, this);
-    this.trigger('createRoom', room);
+    this.emit('createRoom', room);
     room.on('destroy', this.removeRoom.bind(this, room));
   },
 
@@ -31,29 +29,7 @@ utils.extend(Avalon.prototype, {
   login: function (socket, id) {
     user = new User(id, id, socket);
     this.enter(user);
-    user.on('rename', this.onUserRename.bind(this, user));
     user.on('destroy', this.leave.bind(this, user));
-  },
-
-
-  onEnter: function (user) {
-    this.connector.notice('login', user.toString());
-  },
-
-  onUserRename: function (user, name) {
-    this.connector.notice('renameUser', name);
-  },
-
-  createRoomCallback: function (room) {
-    this.connector.notice('createRoom', room.toString());
-  },
-
-  trigger: function (type, data) {
-    this.emit(type, data);
-    var callback = this[type + "Callback" ];
-    if (callback) {
-      callback.call(this, data);
-    }
   },
 });
 
