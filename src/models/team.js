57ef5d1d71e0bc7ac4b3e251fe5020b65e8ff100
Tomoya Chiba <tomo.asleep@gmail.com>
2014-07-15 20:19:30 +0900

    Let Team use object to store its members

--- src/models/team.js
+++ src/models/team.js
@@ -5,7 +5,7 @@ States = ["select_member", "vote", "agree","disagree"]
 
 var Team = function Team(selector,group_sz,voter_sz){
     this.selector = selector;
-    this.group = [];// User Array
+    this.group = {};// User Array
     this.group_sz = group_sz;
     this.voter_map = {};//Teamr map User.id -> Yes/No(true/false)
     this.voter_sz = voter_sz;
@@ -15,37 +15,29 @@ var Team = function Team(selector,group_sz,voter_sz){
 utils.inherit(events.EventEmitter, Team);
 
 Team.prototype.add_group = function(pusher,add_man) {
-    if(this.selector === pusher && this.group.length < group_sz){
-        var add_man_in_group = false;
-        for(var i = 0; i < group.length;i++){
-            if(this.group[i] === add_man){
-                add_man_in_group = true;
-            }
-        }
-        if(!add_man_in_group){
-            this.group.push(add_man);
-        }
-    }
+    // console.log(this.selector.toString(), pusher.toString());
+    if (this.selector !== pusher)
+      throw new Error('Only the selected player can add player to team');
+    var group = Object.values(this.group)
+    if(group.length >= this.group_sz)
+      throw new Error('the team is full');
+
+    this.group[add_man.id] = add_man;
 }
 
 Team.prototype.remove_group = function(remover,remove_man){
-    if(this.selector === remover && this.group.length > 0){
-        var remove_man_index = -1;
-        for(var i = 0; i < group.length;i++){
-            if(this.group[i] === remove_man){
-                remove_man_index = i;
-            }
-        }
-        if(remove_man_index > 0){
-            this.group = this.group.splice(remove_man_index,1);
-        }
-    }
+    if (this.selector !== remover)
+      throw new Error('Only the selected player can remove player from team');
+    var group = Object.values(this.group)
+    if(group.length <= 0)
+      throw new Error('the team is empty');
+    delete this.group[remove_man.id];
 }
 
 Team.prototype.is_vote_success = function(){
     var agree = 0;
     var disagree = 0;
-    if(Object.keys(this.voter_map).length != voter_sz){
+    if(Object.keys(this.voter_map).length != this.voter_sz){
         return false;
     }
     for(var pep in voter_map){
@@ -60,18 +52,20 @@ Team.prototype.is_vote_success = function(){
 
 Team.prototype.change_voter_map = function(voter,vote_res){
     
-    voter_map[voter.id] = vote_res;
+    this.voter_map[voter.id] = vote_res;
+    this.emit('vote', voter, vote_res);
 
 }
 
 Team.prototype.go_vote = function(){
-    if(this.state === States[0] && this.group.length == this.group_sz){
+    if(this.state === States[0] && Object.values(this.group).length == this.group_sz){
         this.state = States[1];
+        this.emit('go:vote');
     }
 }
 
 Team.prototype.judge = function(){
-    if(this.state == States[1] && Object.keys(this.voter_map).length == voter_sz){
+    if(this.state == States[1] && Object.keys(this.voter_map).length == this.voter_sz){
         if(this.is_vote_success()){
             this.state = States[2];
             this.emit("agree");
