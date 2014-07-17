9dc7bac95ff436fead2ff5ddc61b674eef63667e
rosetta-jpn <rosetta.jpn@gmail.com>
2014-07-17 21:55:24 +0900

    HTMLframe with var

--- src/models/quest.js
+++ src/models/quest.js
@@ -2,13 +2,16 @@ var Team = require("./team")
   , utils = require("../utils")
   , events = require("events");
 
+var States = ["NOW","SUCCESS","FAILURE"]
+
 var Quest = function Quest(Game,success_number,team_sz){
-    this.game = Game;
-    this.success_number = success_number;
-    this.team_sz = team_sz;
-    this.vote_count = 0;
-    this.members = [];//User Array
-    this.mission_list = {};//User.id -> Success/Failure(True/False) Hash
+  this.game = Game;
+  this.success_number = success_number;
+  this.team_sz = team_sz;
+  this.result = "NOW";
+  this.vote_count = 0;
+  this.members = [];//User Array
+  this.mission_list = {};//User.id -> Success/Failure(True/False) Hash
 }
 
 utils.inherit(events.EventEmitter, Quest);
@@ -18,56 +21,58 @@ Quest.prototype.start = function () {
 }
 
 Quest.prototype.create_Team = function(){
-    var selector = this.game.nextSelector();
-    this.team = new Team(selector, this.team_sz, this.game.players.length);
-    this.vote_count += 1;
-    this.emit('newTeam', this.team);
-    this.team.on("agree",this.onAgree.bind(this));
-    this.team.on("disAgree",this.onDisAgree.bind(this));
-    return this.team;
+  var selector = this.game.nextSelector();
+  this.team = new Team(selector, this.team_sz, this.game.players.length);
+  this.vote_count += 1;
+  this.emit('newTeam', this.team);
+  this.team.on("agree",this.onAgree.bind(this));
+  this.team.on("disAgree",this.onDisAgree.bind(this));
+  return this.team;
 }
 
 Quest.prototype.judge_success = function(){
-    var success = 0;
-    if(Object.keys(this.members).length == team_sz){
-        for(var pep in mission_list){
-            if(mission_list[pep]){
-                success += 1;
-            }
-        }
-        if(success >= success_number){
-            this.emit("success");
-        }else{
-            this.emit("failure");
-        }
-    }else{
-        this.emit("failure");
+  var success = 0;
+  if(Object.keys(this.members).length == team_sz){
+    for(var pep in mission_list){
+      if(mission_list[pep]){
+        success += 1;
+      }
     }
+    if(success >= success_number){
+      this.state = "SUCCESS";
+      this.emit("success");
+    }else{
+      this.state = "FAILURE"
+      this.emit("failure");
+    }
+  }else{
+    this.emit("failure");
+  }
 }
 
 Quest.prototype.change_mission_list = function(missioner,mission_res){
-    var in_team_group = false;
-    for(var i = 0;i < this.members.length;i++){
-        if(this.members[i] === missioner){
-            in_team_group = true;
-        }
+  var in_team_group = false;
+  for(var i = 0;i < this.members.length;i++){
+    if(this.members[i] === missioner){
+      in_team_group = true;
     }
-    if(in_team_group){
-        mission_list[missioner.id] = mission_res;
-    }
+  }
+  if(in_team_group){
+    mission_list[missioner.id] = mission_res;
+  }
 }
 
 
 
 Quest.prototype.onAgree = function(){
-    this.members = this.team.group;
+  this.members = this.team.group;
 }
 
 Quest.prototype.onDisAgree = function(){
-    if(vote_count == 5){
-        return this.onAgree();
-    }
-    this.create_Team();
+  if(vote_count == 5){
+    return this.onAgree();
+  }
+  this.create_Team();
 }
 
 module.exports = Quest;
--- src/models/team.js
+++ src/models/team.js
@@ -1,79 +1,83 @@
 var utils = require("../utils")
-  , events = require("events");
+, events = require("events");
 
 States = ["select_member", "vote", "agree","disagree"]
 
 var Team = function Team(selector,group_sz,voter_sz){
-    this.selector = selector;
-    this.group = {};// User Array
-    this.group_sz = group_sz;
-    this.voter_map = {};//Teamr map User.id -> Yes/No(true/false)
-    this.voter_sz = voter_sz;
-    this.state = States[0];
+  this.selector = selector;
+  this.group = {};// User Array
+  this.group_sz = group_sz;
+  this.voter_map = {};//Teamr map User.id -> Yes/No(true/false)
+  this.voter_sz = voter_sz;
+  this.state = States[0];
 }
 
 utils.inherit(events.EventEmitter, Team);
 
 Team.prototype.add_group = function(pusher,add_man) {
-    // console.log(this.selector.toString(), pusher.toString());
-    if (this.selector !== pusher)
-      throw new Error('Only the selected player can add player to team');
-    var group = Object.values(this.group)
-    if(group.length >= this.group_sz)
-      throw new Error('the team is full');
+  // console.log(this.selector.toString(), pusher.toString());
+  if (this.selector !== pusher)
+    throw new Error('Only the selected player can add player to team');
+  var group = Object.values(this.group)
+  if(group.length >= this.group_sz)
+    throw new Error('the team is full');
 
-    this.group[add_man.id] = add_man;
+  this.group[add_man.id] = add_man;
 }
 
 Team.prototype.remove_group = function(remover,remove_man){
-    if (this.selector !== remover)
-      throw new Error('Only the selected player can remove player from team');
-    var group = Object.values(this.group)
-    if(group.length <= 0)
-      throw new Error('the team is empty');
-    delete this.group[remove_man.id];
+  if (this.selector !== remover)
+    throw new Error('Only the selected player can remove player from team');
+  var group = Object.values(this.group)
+  if(group.length <= 0)
+    throw new Error('the team is empty');
+  delete this.group[remove_man.id];
 }
 
 Team.prototype.is_vote_success = function(){
-    var agree = 0;
-    var disagree = 0;
-    if(Object.keys(this.voter_map).length != this.voter_sz){
-        return false;
+  var agree = 0;
+  var disagree = 0;
+  if(Object.keys(this.voter_map).length != this.voter_sz){
+    return false;
+  }
+  for(var pep in voter_map){
+    if(voter_map[pep]){
+      agree += 1;
+    }else{
+      disagree += 1;
     }
-    for(var pep in voter_map){
-        if(voter_map[pep]){
-            agree += 1;
-        }else{
-            disagree += 1;
-        }
-    }
-    return agree > disagree;
+  }
+  return agree > disagree;
 }
 
 Team.prototype.change_voter_map = function(voter,vote_res){
-    
-    this.voter_map[voter.id] = vote_res;
-    this.emit('vote', voter, vote_res);
+  
+  this.voter_map[voter.id] = vote_res;
+  this.emit('vote', voter, vote_res);
 
 }
 
 Team.prototype.go_vote = function(){
-    if(this.state === States[0] && Object.values(this.group).length == this.group_sz){
-        this.state = States[1];
-        this.emit('go:vote');
-    }
+  if(this.state === States[0] && Object.values(this.group).length == this.group_sz){
+    this.state = States[1];
+    this.emit('go:vote');
+  }
 }
 
 Team.prototype.judge = function(){
-    if(this.state == States[1] && Object.keys(this.voter_map).length == this.voter_sz){
-        if(this.is_vote_success()){
-            this.state = States[2];
-            this.emit("agree");
-        }else{
-            this.state = States[3];
-            this.emit("disAgree");
-        }
+  if(this.state == States[1] && Object.keys(this.voter_map).length == this.voter_sz){
+    if(this.is_vote_success()){
+      this.state = States[2];
+      this.emit("agree");
+    }else{
+      this.state = States[3];
+      this.emit("disAgree");
     }
+  }
+}
+
+Team.prototype.isApprove = function(){
+  return this.state == States[2];// Approve
 }
 
 
